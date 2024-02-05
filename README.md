11ab922487bce88697cd2f61246bd8b68ed9691b
Courtney Sims <courtney.sims@wpengine.com>
2024-02-05 13:49:05 -0600

    comment updates

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # go-scraper
 
 A terminal application for scraping web pages about shelter pets.
+
+This is the first project in my Year of Go. Comments hold tidbits I learn as I code.
--- scrape.go
+++ scrape.go
@@ -7,14 +7,12 @@ import (
 )
 
 func makeRequest() error {
-	// manages the network communication and is responsible for the execution of the attached callbacks while a collector job is running. To work with colly, you have to initialize a Collector
-	// short declarations are only possible inside a function
+	// Colly learning: manages the network communication and is responsible for the execution of the attached callbacks while a collector job is running. To work with colly, you have to initialize a Collector
+	// Go learning: short declarations are only possible inside a function
 	c := colly.NewCollector()
 
 	// Request starts a collector job by creating a custom HTTP request
 	method := "GET"
-	// Get dogs from multiple shelters and add information about all of them in a csv
-	// Return the oldest dog
 	URL := "https://austinpetsalive.org/adopt/dogs"
 
 	c.OnRequest(func(r *colly.Request) {
